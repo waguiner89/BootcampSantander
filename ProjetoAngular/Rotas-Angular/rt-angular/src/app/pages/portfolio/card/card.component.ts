337ed9fbec3d39c1ee7338e6f783aec0879f9781
Wagner C. Vaz <wagner.tech89@hotmail.com>
2023-10-06 16:53:46 -0300

    Novo certificado e alguns projetos simples sobre Angular

---
+++ ProjetoAngular/Rotas-Angular/rt-angular/src/app/pages/portfolio/card/card.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  constructor (
+      private parametrizador: ActivatedRoute,
+      private navegador: Router
+    ){
+
+    this.parametrizador.params.subscribe(
+      res => console.log(res)
+    )
+
+    this.parametrizador.firstChild?.params.subscribe(
+      res => console.log(res)
+    )
+
+    this.parametrizador.queryParams.subscribe(
+      res => console.log(res)
+    )
+
+  }
+
+  ngOnInit(): void {
+    //setInterval(() =>{
+      //this.navegador.navigate(['/'])
+    //}, 5000)
+  }
+}
